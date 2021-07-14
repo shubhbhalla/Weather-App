e75fd6ae10a353ab07fae5497af28704104a25f0
Shubh Bhalla <67706402+shubhbhalla@users.noreply.github.com>
2021-07-13 22:11:21 -0700

    final product

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # Weather-App
+
+## This project was made for a coding assignment given by [Orange Topi](https://www.orangetopi.com/).
+
+- Used [Firebase Auth](https://firebase.google.com/products/auth) for authentication of users.
+- Used [Firebase Firestore](https://firebase.google.com/products/firestore) for data persistance.
+- Used [Styled components](https://styled-components.com/) for css styling.
+- Used [React-Google-Charts](https://react-google-charts.com/) for chart visualization.
+- Utilized [Open Weather Api](https://openweathermap.org/) for current and historical weather data.
+- Deployed app through [Heroku CLI](https://devcenter.heroku.com/).
--- src/Header.jsx
+++ src/Header.jsx
@@ -1,9 +1,10 @@
 import { Button } from '@material-ui/core';
+import Chip from '@material-ui/core/Chip';
+
 import SearchField from './SearchField';
+import { MainContext } from './MainContext';
 
 import { useContext } from 'react';
-
-import { MainContext } from './MainContext';
 
 const Header = () => {
   const { signOut, email } = useContext(MainContext);
@@ -13,8 +14,14 @@ const Header = () => {
         display: 'flex',
         justifyContent: 'space-evenly',
         color: 'red',
+        alignItems: 'center',
       }}
     >
+      <Chip
+        label="Click on the weather cards to display graph!"
+        color="primary"
+        variant="outlined"
+      />
       <SearchField />
       <Button onClick={signOut}>Sign Out, {email}</Button>
     </div>
